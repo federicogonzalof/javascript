b41d3bd3174d94676c94e2f31ac1f3bb40b7f7f5
fgfernandez <federicogonzalof@gmail.com>
2022-07-11 22:22:02 -0300

    eventos

---
+++ js/peliculas.js
@@ -0,0 +1,11 @@
+const productos = [
+    {id:01, genero:"Biografia", nombre:"Elvis", precio: 500, foto: "https://es.web.img2.acsta.net/pictures/22/05/18/14/52/1524445.jpg" },
+    {id:02, genero:"Terror", nombre:"El telefono negro", precio: 1300, foto: "https://mx.web.img3.acsta.net/pictures/22/05/09/23/04/5145567.jpg" },
+    {id:03, genero:"Aventuras", nombre:"Jurassic World: Dominio", precio: 1800, foto: "https://es.web.img2.acsta.net/pictures/22/04/25/10/39/2475018.jpg" },
+    {id:04, genero:"Animacion", nombre:"La gallina turuleca", precio: 1000, foto: "https://pics.filmaffinity.com/La_gallina_Turuleca-543555397-large.jpg" },
+    {id:05, genero:"Animacion", nombre:"Lightyear", precio: 2000, foto: "https://pics.filmaffinity.com/Lightyear-529062623-mmed.jpg" },
+    {id:06, genero:"Animacion", nombre:"Minions, nace un villano", precio: 1500, foto: "https://cloudfront-us-east-1.images.arcpublishing.com/abccolor/YMO2J6OVS5FFJP2TMCCU6AAVBE.jpg" },
+    {id:07, genero: "Aventuras", nombre: "Thor amor y trueno", precio: 3000, foto: "https://static.cinepolis.com/resources/mx/movies/posters/414x603/39361-213594-20220706102528.jpg"},
+    {id:08, genero: "Accion", nombre: "Todo en todas partes al mismo tiempo", precio: 1000, foto: "https://static.cinepolis.com/resources/mx/movies/posters/414x603/39349-815954-20220512044043.jpg"},
+    {id:09, genero: "Accion", nombre: "Top gun maverick", precio: 3000, foto: "https://pics.filmaffinity.com/Top_Gun_Maverick-537976462-large.jpg"},
+];
